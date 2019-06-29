c1c37ff3f26f3bca3d1c17ed90aa6561f2a836e2
visheshRajani <vrajani8055@gmail.com>
2019-06-30 05:14:34 +0530

    final

--- bank-api/src/main/java/com/example/bankapi/service/JwtUserDetailsService.java
+++ bank-api/src/main/java/com/example/bankapi/service/JwtUserDetailsService.java
@@ -1,7 +1,7 @@
 package com.example.bankapi.service;
 
 
-import java.util.Collection;
+import java.util.ArrayList;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -16,15 +16,12 @@ public class JwtUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		if ("admin".equals(username)) {
-			return new User("admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
-					emptyList());
+			ArrayList<GrantedAuthority> arraylist=new ArrayList<>();
+			return new User("admin", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",arraylist);
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
 		}
 	}
 
-	private Collection<? extends GrantedAuthority> emptyList() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
 }
